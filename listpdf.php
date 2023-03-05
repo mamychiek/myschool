28a222914cee16afd30b827d2f1da0d9cd1dabf9
mamy <mamybhwr@gmail.com>
2023-03-05 01:03:29 -0800

    Add files via upload

---
+++ listpdf.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/style.css" rel="stylesheet">
+    
+</head>
+<body>
+    <div>
+        <form>
+            <main>
+                <table id="tbl"></table>
+                <tr>
+                    <th> اسم المستند</th>
+                    <th> التفاصيل</th>
+                    <th> الرابط</th>
+                </tr>
+            </main>
+        </form>
+    </div>
+</body>
+</html>
